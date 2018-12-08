9d86c6c38ee1c4598eeab876ad52c411121bd4b3
Gaurav Madaan <gaurav.madaan@technossus.com>
2018-12-08 23:00:20 +0530

    Added docs

--- angular-app-with-docs/src/app/math-operations.service.ts
+++ angular-app-with-docs/src/app/math-operations.service.ts
@@ -5,12 +5,36 @@ import { Injectable } from '@angular/core';
 })
 export class MathOperationsService {
 
+  /**
+   * @ignore
+   */
   constructor() { }
 
+  /**
+   * Performs addition of two numbers.
+   * 
+   * @example 
+   * getAddition(1, 2)
+   * 
+   * @param {number} a First Number
+   * @param {number} b Second Number
+   * @returns The sum of two numbers
+   */
   getAddition(a: number, b: number): number{
     return a + b;
   }
 
+
+  /**
+   * Performs subtraction of two numbers.
+   * 
+   * @example 
+   * getSubtraction(1, 2)
+   * 
+   * @param {number} a First Number
+   * @param {number} b Second Number
+   * @returns The subtraction of two numbers
+   */  
   getSubtraction(a: number, b: number): number{
     return a - b;
   }
